09b7eb98527681470b10806197eadb62f90c0d10
Adama M Sombie <asombie@gmail.com>
2020-04-03 15:44:08 -0400

    Revert "Second commit"
    This reverts commit 8345a6f7b50eac035269145168c494b4f7b0863e.

--- manifests/params.pp
+++ manifests/params.pp
@@ -4,6 +4,10 @@ class ntp::params {
   $package_name = hiera('ntp::package_name')
   $package_ensure = hiera('ntp::package_ensure')
   $config_name = hiera('ntp::config_name'
+  $config_file_mode = hiera('ntp::config_file_mode')
+  $servers = hiera('ntp::servers')
+  $service_ensure = hiera('ntp::service_ensure')
+  $service_hasrestart = hiera('ntp::service_hasrestart')
   $service_enable = hiera('ntp::service_enable')
   $service_hasstatus = hiera('ntp::service_hasstatus')
 
